9d24b5a0efb4fece7902d7bed51b76b0c0160b11
aavashb <baralaavas@gmail.com>
2022-10-28 15:48:41 +0545

    bugs fixed

--- index.js
+++ index.js
@@ -26,61 +26,71 @@ function checker() {
   }
 }
 
-
 function runTimer() {
-  var hour = document.getElementById("hr").value;
-  var minute = document.getElementById("min").value;
-  var second = document.getElementById("sec").value;
+  var hour = +document.getElementById("hr").value;
+  var minute = +document.getElementById("min").value;
+  var second = +document.getElementById("sec").value;
   var count = 0;
 
   function run() {
     if (timer) {
       count++;
 
-      if (count === 100) {
-        second--;
-        count = 0;
-      }
+      if (hour == 0 && minute == 0 && (second == 0|| second == 1)) {
+        timer = false;
+        setTimeout(()=>{
+          document.getElementById("dispHr").innerHTML = "00";
+          document.getElementById("dispMin").innerHTML = "00";
+          document.getElementById("dispSec").innerHTML = "00";
+        },1000)
+        
+      } else {
+        if (count === 100) {
+          second--;
+          
+          count = 0;
 
-      if (second == 0) {
-        minute--;
-        second = 59;
-      }
+          if (second == 0) {
+            minute--;
+            second = 59;
 
-      if (minute == 0) {
-        hour--;
-        minute = 59;
-        second = 59;
-      }
-      let hrString = hour;
-      let minString = minute;
-      let secString = second;
-      let countString = count;
+            if (minute == 0) {
+              hour--;
+              minute = 59;
+              second = 59;
+            }
+          }
+        }
 
-      if (hour < 10) {
-        hrString = "0" + hrString;
-      }
+        let hrString = hour;
+        let minString = minute;
+        let secString = second;
+        let countString = count;
 
-      if (minute < 10) {
-        minString = "0" + minString;
-      }
+        if (hour < 10) {
+          hrString = "0" + hrString;
+        }
 
-      if (second < 10) {
-        secString = "0" + secString;
-      }
+        if (minute < 10) {
+          minString = "0" + minString;
+        }
 
-      if (count < 10) {
-        countString = "0" + countString;
-      }
+        if (second < 10) {
+          secString = "0" + secString;
+        }
 
-      document.getElementById('dispHr').innerHTML = hrString;
-      document.getElementById('dispMin').innerHTML = minString;
-      document.getElementById('dispSec').innerHTML = secString;
+        if (count < 10) {
+          countString = "0" + countString;
+        }
+
+        document.getElementById("dispHr").innerHTML = hrString;
+        document.getElementById("dispMin").innerHTML = minString;
+        document.getElementById("dispSec").innerHTML = secString;
+      }
       setTimeout(run, 10);
     }
   }
   return run();
-
 }
 
 runTimer();
